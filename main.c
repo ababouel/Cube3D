c1ec5d790cfbb5406011172bc0404f84501c2f1f
Abdelghani Abouelmahassine <ababouel@e3r7p9.1337.ma>
2022-08-06 14:19:43 +0100

    remove all include and libs mlx

--- main.c
+++ main.c
@@ -12,9 +12,23 @@
 
 #include "mlx.h"
 #include <math.h>
+#include <stdlib.h>
 
-typedef struct	s_data {
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
 	void	*img;
+}				t_vars;
+typedef struct	s_data {
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
@@ -28,28 +42,46 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
 
+int	rans(int max_number, int minimum_number)
+{
+	int number;
+
+	number = rand() % (max_number + 1 - minimum_number) + minimum_number;
+	return (number);
+}
+int	close(int keycode, t_vars *vars)
+{
+	(void)keycode;
+	(void)vars;
+	exit(0);
+}
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_vars	vars;
 	t_data	img;
 
-	mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1280, 720, "Hello world!");
-	img.img = mlx_new_image(mlx, 1280, 720);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	vars.mlx = mlx_init();
+	if (vars.mlx != NULL)
+    	vars.win = mlx_new_window(vars.mlx, 1280, 720, "cub3d");
+	vars.img = mlx_new_image(vars.mlx, 1280, 720);
+	img.addr = mlx_get_data_addr(vars.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	int x = 1;
 	while (x < 720)
 	{
 		int y = 1;
 		while (y < 1280)
 		{
-			my_mlx_pixel_put(&img, y, x, 0x00FF0000);
+			my_mlx_pixel_put(&img, y, x, create_trgb(128, rans(255, 0), rans(255, 0), rans(255, 0)));
 			y++;
 		}
 		x++;
 	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
+	mlx_hook(vars.win, ON_DESTROY, 1L<<0, close, &vars);
+	mlx_loop(vars.mlx);
 }
