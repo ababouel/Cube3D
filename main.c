83bd98c6c89003d5dd7ceab7e17168d4aef406f8
Abdelghani Abouelmahassine <ababouel@e1r4p12.1337.ma>
2022-10-09 20:33:12 +0100

    chores: create function generate pixels

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 20:34:40 by ababouel          #+#    #+#             */
-/*   Updated: 2022/10/08 03:47:20 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/10/09 20:21:25 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,34 @@ int	ft_init_vars(t_vars *vars)
 	vars->data->hgt = 10;
 	vars->data->wth = 10;
 	vars->ceil = add_color(225, 30, 0, 0);
-	vars->floor = add_color(220, 100, 0, 0);
-	
+	vars->floor = add_color(220, 100, 0, 0);	
 	return(0);
+}
+
+uint32_t	*generate_pixels(t_vars *vars, char *path)
+{
+	t_point		t;
+	t_imgarg	ig;
+	char		*ttc;
+	uint32_t	*txtcolor;
+
+	t.py = 0;
+	ig = vars->txtre.ig;
+	ig.img = mlx_xpm_file_to_image(vars->mlx, path, &vars->txtre.width, &vars->txtre.height);
+	ig.addr = mlx_get_data_addr(ig.img, &ig.bpp, &ig.line_len, &ig.endian);
+	txtcolor = malloc(sizeof(uint32_t) * (uint32_t)vars->txtre.width * (uint32_t) vars->txtre.height);
+	while (t.py <= vars->txtre.height)
+	{
+		t.px = 0;
+		while (t.px <= vars->txtre.width)
+		{
+			ttc = ig.addr + ((int) t.py * ig.line_len + (int) t.px * (ig.bpp / 8));
+			txtcolor[(vars->txtre.width * t.py) + t.px] = ttc;
+			t.px++;	
+		}
+		t.py++;
+	}
+	return (txtcolor);
 }
 
 int    ft_init(t_vars *vars)
@@ -66,62 +91,19 @@ int    ft_init(t_vars *vars)
 			WINDOW_WIDTH, WINDOW_HEIGHT);
 	vars->iarg->addr = mlx_get_data_addr(vars->iarg->img,
 			&vars->iarg->bpp, &vars->iarg->line_len, &vars->iarg->endian);
-	vars->txtre.ig.img = mlx_xpm_file_to_image(vars->mlx, "./texture/wall.xpm", &vars->txtre.width, &vars->txtre.height);
-	vars->txtre.ig.addr = mlx_get_data_addr(vars->txtre.ig.img, &vars->txtre.ig.bpp, &vars->txtre.ig.line_len, &vars->txtre.ig.endian);	
 	return (0);
-}
-
-static void	generate_pixel(t_vars *vars)
-{
-	t_texture	txt;
-	t_point		t;
-	t_point		p;
-	char		*dst;
-	char		*ttc;
-
-	p.px = 0;
-	t.px = 0;
-	txt = vars->txtre;
-	while(p.px <= WINDOW_WIDTH)
-	{
-		t.py = 0;
-		p.py = 0;
-		if (t.px > 60)
-			t.px = 0;
-		while (p.py <= WINDOW_HEIGHT)
-		{
-			if (t.py > 60)
-				t.py = 0;
-			dst  = vars->iarg->addr + ((int) p.py * vars->iarg->line_len + (int) p.px * (vars->iarg->bpp / 8));
-			ttc = txt.ig.addr + ((int) t.py * txt.ig.line_len + (int) t.px * (txt.ig.bpp / 8));
-			*(unsigned int *)dst = *(unsigned int *)ttc;
-			t.py++;
-			p.py++;
-		}
-		t.px++;
-		p.px++;
-	}
 }
 
 int render_next_frame(void *vars)
 {
 	t_vars	*v;
-	// int		tx;
-	// int 	x;
-
-	// x = 0;
-	// tx = 0;
+	
 	v = (t_vars *)vars;
 	climg(v->iarg->img);
-	// while(x <= WINDOW_WIDTH)
-	// {
-	// 	if (tx > 60)
-	// 		tx = 0;
-		generate_pixel(vars);
-		// x++;
-		// tx++;	
-	// }
-	mlx_put_image_to_window( v->mlx, v->win, v->iarg->img, 0,0);
+	draw_ceil_floor(vars);
+	draw_map(v);
+	camera(v);
+	mlx_put_image_to_window( v->mlx, v->win, v->iarg->img, 0, 0);
 	return (1);
 }
 
@@ -133,13 +115,8 @@ int	main(void)
 	ft_init_vars(vars);
 	ft_init(vars);
 	mlx_loop_hook(vars->mlx, render_next_frame, (void *)vars);	
-	
 	mlx_key_hook(vars->win, esc_key, vars);
 	mlx_hook(vars->win, 17, 0, close_game, vars);
 	mlx_do_sync(vars->mlx);
 	mlx_loop(vars->mlx);
-}	
-	// draw_ceil_floor(vars);
-	// draw_map(v);
-	// camera(v);
-	// mlx_put_image_to_window( v->mlx, v->win, v->iarg->img, 0,0);
+}
--- raycast/camera.c
+++ raycast/camera.c
@@ -13,60 +13,36 @@
 #include "raycast.h"
 #include "tools.h"
 
-// static void	draw_pixels(t_vars *data, t_vector *v, unsigned int color)
-// {
-// 	char		*dst;
-// 	t_imgarg	img;
+static void	draw_pixels(t_vars *data, t_vector *v, unsigned int color)
+{
+	char		*dst;
+	t_imgarg	img;
 
-// 	img = *data->iarg;
-// 	if (v->x < WINDOW_WIDTH && v->x >= 0 && v->y < WINDOW_HEIGHT && v->y >= 0)
-// 	{
-// 		dst = img.addr + ((int) v->y * img.line_len
-// 				+ (int)v->x * (img.bpp / 8));
-// 		*(unsigned int *) dst = color;
-// 	}	
-// }
+	img = *data->iarg;
+	if (v->x < WINDOW_WIDTH && v->x >= 0 && v->y < WINDOW_HEIGHT && v->y >= 0)
+	{
+		dst = img.addr + ((int) v->y * img.line_len
+				+ (int)v->x * (img.bpp / 8));
+		*(unsigned int *) dst = color;
+	}	
+}
 
-// static void xpm_image(char *xpm, t_vars *vars)
-// {
-//     vars->txtre.ig = malloc(sizeof(t_imgarg));
-//     vars->txtre.ig->img = mlx_xpm_file_to_image(vars->mlx, xpm, &vars->txtre.width,
-//             &vars->txtre.height);
-//     vars->txtre.ig->addr = mlx_get_data_addr(vars->txtre.ig->img, &vars->txtre.ig->bpp,
-//          &vars->txtre.ig->line_len, &vars->txtre.ig->endian);
-//     vars->txtre.ig->bpp /= 8;
-//     vars->txtre.width = vars->txtre.ig->line_len / vars->txtre.ig->bpp;
-//     vars->txtre.height = vars->txtre.ig->line_len / vars->txtre.ig->bpp;
-// }
-
-// static  unsigned int generate_color(char * path, int* y, t_vars *vars)
-// {
-//     // int             offset_x;
-//     (void)y;
-//     xpm_image(path, vars);
-//     if (vars->ray.is_vertical == 1)
-//     {
-//         // offset_x = (int) vars->ray.inters.current_pos.y % 60;
-//         return (*(int *)(unsigned char [4]){199, 209, 209, 0});
-//     }
-//     else
-//     {
-//         // offset_x = (int) vars->ray.inters.current_pos.x % (int)RECT_SIZE;
-//         return (*(int *)(unsigned char [4]){183, 231, 255, 0}); 
-//     }
-//     // dst = vars->txtre.ig->addr + ((int) (*y) * vars->txtre.width) 
-//             // + ((int) offset_x * vars->txtre.ig->bpp);
-// }
-
-void    draw_wall(double dis_ray, t_vars *vars, int x)
+void    draw_wall(double dis_ray, t_vars *vars, int *x, int *tx)
 {
     t_vector        v;
     int             top_y;
+    int             txt_y;
     int             bottom_y;
     double          distance;
     int             wall_height;
+    char            *ttc;
+    t_imgarg        ig;
 
     top_y = 0;
+    txt_y = 0;
+    if (*tx > 60)
+        *tx = 0;
+    ig = vars->txtre.ig;
     distance = (WINDOW_WIDTH / 2) / tan(M_PI / 6);
     wall_height = (int)((RECT_SIZE / dis_ray) * distance);
     top_y = (int)WINDOW_HEIGHT / 2 - wall_height / 2;
@@ -75,25 +51,27 @@ void    draw_wall(double dis_ray, t_vars *vars, int x)
         top_y = 0;
     if (bottom_y > WINDOW_HEIGHT)
         bottom_y = WINDOW_HEIGHT;
-    v.x = x;
+    v.x = *x;
     while (top_y <= bottom_y)
     {
         v.y = top_y;
-        if (vars->ray.is_vertical == 1)
-            v.color = add_color(209, 209, 199, 0);
-        else
-            v.color = add_color(255,231,183,0);         
-        draw_pixel(vars, &v);
+        if (txt_y > 60)
+            txt_y = 0;
+        ttc = ig.addr + ((int) txt_y * ig.line_len + (int) *tx * (ig.bpp / 8));
+        draw_pixels(vars, &v, *(unsigned int *) ttc);
         top_y++;
+        txt_y++;
     }
 }
 void    camera(t_vars *vars)
 {
     int x;
+    int tx;
     double dis;
     double pdis;
 
     x = 0;
+    tx = 0;
     dis = 0.0;
     vars->ray.origin = *vars->ordr.origin;
     vars->ray.dir = *vars->ordr.minplane;
@@ -102,9 +80,10 @@ void    camera(t_vars *vars)
     {
         dis = cast_ray(vars);
         pdis = dis * cos(vars->ray.dir.angle - vars->ordr.dir1->angle);
-        draw_wall(pdis, vars, x);
+        draw_wall(pdis, vars, &x, &tx);
         vars->ray.dir.angle= (1.0 / (WINDOW_WIDTH)) * (M_PI / 6.0);
         rotation(&vars->ray.dir, vars->ray.dir.angle); 
         x++;
+        tx++;
     }
 }
