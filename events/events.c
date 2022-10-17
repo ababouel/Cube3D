e0d537cbfa568b95b3c5d4a7d62a304525174111
Farouk Ech-charef <fech-cha@e1r4p6.1337.ma>
2022-10-17 03:55:18 +0100

    feat: keys for player mouvement

--- events/events.c
+++ events/events.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 02:35:59 by fech-cha          #+#    #+#             */
-/*   Updated: 2022/10/15 21:56:21 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/10/17 03:48:50 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,29 +26,27 @@ int	esc_key(int keynum, t_vars *vars)
 	if (keynum == ESC_KEY)
 		clear_data(vars);
 	if (keynum == ARROWRIGHT)
-	{ 
-		climg(vars->iarg);
+	{
+		vars->ordr.origin->angle += vars->ordr.dir1->angle;
 		rotation(vars->ordr.dir1, vars->ordr.dir1->angle);
 		rotation(vars->ordr.minplane, vars->ordr.minplane->angle);
 		rotation(vars->ordr.maxplane, vars->ordr.maxplane->angle);
 	}
 	else if (keynum == ARROWLEFT)
 	{
-		climg(vars->iarg);
+		vars->ordr.origin->angle -= vars->ordr.dir1->angle;
 		rotation(vars->ordr.dir1, -vars->ordr.dir1->angle);
 		rotation(vars->ordr.maxplane, -vars->ordr.maxplane->angle);
 		rotation(vars->ordr.minplane, -vars->ordr.minplane->angle);
 	}
-	else if (keynum == ARROWUP)
-	{
-		climg(vars->iarg);
+	else if (keynum == UP_W)
 		vect_add(vars->ordr.origin, vars->ordr.dir1, 10);
-	}
-	else if (keynum == ARROWDOWN)
-	{
-		climg(vars->iarg);
-		vect_add(vars->ordr.origin, vars->ordr.dir1, -10);	
-	}
+	else if (keynum == DOWN_S)
+		vect_add(vars->ordr.origin, vars->ordr.dir1, -10);
+	else if (keynum == LEFT_A)
+		move_dir(vars->ordr.origin, 10);
+	else if (keynum == RIGHT_D)
+		move_dir(vars->ordr.origin, -10);
 	return (0);
 }
 
--- tools/vect_tools.c
+++ tools/vect_tools.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vect_tools.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fech-cha <fech-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 10:44:01 by ababouel          #+#    #+#             */
-/*   Updated: 2022/10/16 04:58:19 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/10/17 03:49:01 by fech-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,16 @@ void    vect_add(t_vector *origin,t_vector *dir, double travel)
 
     new_origin.x = origin->x + travel * dir->x;
     new_origin.y = origin->y + travel * dir->y;
+    origin->x = new_origin.x;
+    origin->y = new_origin.y;
+}
+
+void    move_dir( t_vector *origin, double travel)
+{
+    t_vector    new_origin;
+  
+    new_origin.x = origin->x + travel *  cos(origin->angle);
+    new_origin.y = origin->y + travel * sin(origin->angle);
     origin->x = new_origin.x;
     origin->y = new_origin.y;
 }
